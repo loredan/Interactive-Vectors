70da94e2c31ab20e3154ceff41ba89bab0cd1460
Всеволод Меренков <v.merenkov@letnyayashkola.org>
2026-06-15 03:06:37 +0300

    Line and arrow prefabs

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteAlways]
+[RequireComponent(typeof(Line))]
+public class Arrow : MonoBehaviour
+{
+    [SerializeField] private Vector3 start = Vector3.zero;
+    [SerializeField] private Vector3 end = Vector3.right;
+    [SerializeField] private Color color = Color.red;
+    [Space] [SerializeField] private Image arrowhead;
+    [SerializeField] private float stemOffset;
+
+    private Line _line;
+
+
+    private void Awake()
+    {
+        _line = GetComponent<Line>();
+        UpdateArrow();
+    }
+
+    private void OnValidate()
+    {
+        UpdateArrow();
+    }
+
+    private void UpdateArrow()
+    {
+        if (_line == null) return;
+
+        ((RectTransform)arrowhead.transform).anchoredPosition3D = Vector3.right * stemOffset;
+        arrowhead.transform.rotation = Quaternion.LookRotation(end - start) * Quaternion.AngleAxis(-90, Vector3.up);
+        arrowhead.color = color;
+        _line.SetEnd(end - Vector3.Normalize(end - start) * stemOffset);
+        _line.SetColor(color);
+    }
+}
--- Assets/Scripts/Line.cs
+++ Assets/Scripts/Line.cs
@@ -1,16 +1,55 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
+[ExecuteAlways]
+[RequireComponent(typeof(RectTransform))]
+[RequireComponent(typeof(Image))]
 public class Line : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Vector3 start = Vector3.zero;
+    [SerializeField] private Vector3 end = Vector3.right;
+    [SerializeField] private Color color = Color.white;
+    [SerializeField] private float width = 0.1f;
+
+    private RectTransform _rectTransform;
+    private Image _image;
+
+    void Awake()
     {
+        _rectTransform = GetComponent<RectTransform>();
+        _image = GetComponent<Image>();
         
+        _rectTransform.pivot = new Vector2(0, 0.5f);
+        UpdateLine();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnValidate()
     {
-        
+        UpdateLine();
+    }
+
+    private void UpdateLine()
+    {
+        if (_image == null || _rectTransform == null) return;
+
+        _rectTransform.anchoredPosition3D = start;
+        _rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, width);
+        _rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, (end - start).magnitude);
+        _rectTransform.rotation = Quaternion.LookRotation(end - start) * Quaternion.AngleAxis(-90, Vector3.up);
+
+        _image.color = color;
+    }
+
+    public void SetEnd(Vector3 value)
+    {
+        end = value;
+        UpdateLine();
+    }
+
+    public void SetColor(Color value)
+    {
+        color = value;
+        UpdateLine();
     }
 }
