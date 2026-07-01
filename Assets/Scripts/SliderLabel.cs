d3184d23a166f392963419194f07140b9bb3878d
Vsevolod Merenkov <loredan725@gmail.com>
2026-07-01 20:58:33 +0300

    Added multiplication operation

---
+++ Assets/Scripts/SliderLabel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(TMP_Text))]
+public class SliderLabel : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    
+    private TMP_Text text;
+
+    private void Awake()
+    {
+        text = GetComponent<TMP_Text>();
+    }
+
+    private void Update()
+    {
+        text.text = slider.value.ToString("F");
+    }
+}
